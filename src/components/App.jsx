02d93b8019e21c0bac1995aeec9b50e98950868e
RafalCeglinsk <123418749+RafalCeglinsk@users.noreply.github.com>
2023-11-05 19:06:18 +0100

    Components
    Cloned components from previous homework.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,97 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactList/ContactList';
+import { Filter } from './Filter/Filter';
+export class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+  componentDidMount() {
+    const savedContacts = localStorage.getItem('contacts');
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.contacts !== this.state.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  handleChangeName = e => {
+    this.setState({ name: e.target.value });
+  };
+
+  handleChangeNumber = e => {
+    this.setState({ number: e.target.value });
+  };
+
+  handleFilterContact = e => {
+    this.setState({ filter: e.target.value });
+  };
+  handleDeleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const { name, number, existingContact } = this.state;
+    if (name.trim() === '' || number.trim() === '') {
+      return;
+    }
+    if (existingContact.has(name)) {
+      alert(`${name} is already in contacts.`);
+    }
+    const newContact = {
+      id: nanoid(),
+      name: name,
+      number: number,
+    };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+      name: '',
+      number: '',
+      existingContact: prevState.existingContact.add(name),
+    }));
+  };
+
+  render() {
+    const { contacts, name, number, filter } = this.state;
+
+    const filteredContacts = contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    return (
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm
+          name={name}
+          number={number}
+          onChangeName={this.handleChangeName}
+          onChangeNumber={this.handleChangeNumber}
+          onSubmit={this.handleSubmit}
+        />
+
+        <h2>Contacts</h2>
+        <Filter
+          filter={filter}
+          handleFilterContact={this.handleFilterContact}
+        />
+        <ContactList
+          filteredContacts={filteredContacts}
+          deleteContact={this.handleDeleteContact}
+        />
+      </div>
+    );
+  }
+}
